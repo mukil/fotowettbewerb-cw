729604430100712fdc413341b339eb7f67961081
Malte Reißig <malte@deepamehta.org>
2013-10-16 10:36:57 +0200

    fixes: setCookie()-call

--- src/main/java/de/kiezatlas/plugins/view/maps/MapsPlugin.java
+++ src/main/java/de/kiezatlas/plugins/view/maps/MapsPlugin.java
@@ -1,21 +1,30 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package de.kiezatlas.plugins.view.maps;
 
 import de.deepamehta.core.osgi.PluginActivator;
 import de.deepamehta.core.service.ClientState;
 import java.io.InputStream;
+import java.util.logging.Logger;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+
+/**
+ * Kiezatlas New (Mobile) Maps 1.0
+ *
+ * Depending on Kiezatlas 2.1.1 and DeepaMehta 4.1.
+ *
+ * @since 26. September 2013
+ * @author Malte Reißig <malte@mikromedia.de>, 2013
+ *
+ */
 
 @Path("/")
 @Produces(MediaType.TEXT_HTML)
 public class MapsPlugin extends PluginActivator {
 
+    private Logger log = Logger.getLogger(getClass().getName());
+
     @GET
-    @Path("/")
     @Produces(MediaType.TEXT_HTML)
     public InputStream getStartView(@HeaderParam("Cookie") ClientState clientState) {
         return invokeStartView();
